2f3e9a8fc68615b620221f0ebdedba3a51c0e06e
StegVerse <210078727+StegVerse@users.noreply.github.com>
2026-08-22 23:43:19 -0500

    Test disclosure and signature transition prerequisites

--- tests/test_verfi_external_formalism.py
+++ tests/test_verfi_external_formalism.py
@@ -11,26 +11,13 @@ SPEC.loader.exec_module(MODULE)
 
 
 class VerFiExternalFormalismTests(unittest.TestCase):
-    def test_clean_sequence_is_candidate_allow(self):
-        case = {
+    def base_case(self):
+        return {
             "id": "CLEAN_SEQUENCE",
             "presented_hash": "doc:v1",
             "authorized_hash": "doc:v1",
+            "disclosure": True,
             "comprehension_evidence": "DISTINGUISHABLE",
-            "authorization_valid_at_commit": True,
-            "evidence_integrity": True,
-            "temporal_order_valid": True,
-            "minimum_information_satisfied": True,
-            "reconstructable": True,
-        }
-        self.assertEqual(MODULE.evaluate(case), "ALLOW_CANDIDATE")
-
-    def test_signature_cannot_substitute_for_comprehension(self):
-        case = {
-            "id": "COMPREHENSION_MISSING",
-            "presented_hash": "doc:v1",
-            "authorized_hash": "doc:v1",
-            "comprehension_evidence": "ABSENT",
             "authorization_valid_at_commit": True,
             "signature": True,
             "evidence_integrity": True,
@@ -38,62 +25,49 @@ class VerFiExternalFormalismTests(unittest.TestCase):
             "minimum_information_satisfied": True,
             "reconstructable": True,
         }
+
+    def test_clean_sequence_is_candidate_allow(self):
+        self.assertEqual(MODULE.evaluate(self.base_case()), "ALLOW_CANDIDATE")
+
+    def test_missing_disclosure_cannot_be_allowed(self):
+        case = self.base_case()
+        case["disclosure"] = False
+        self.assertEqual(MODULE.evaluate(case), "DISCLOSURE_NOT_ESTABLISHED")
+
+    def test_signature_cannot_substitute_for_comprehension(self):
+        case = self.base_case()
+        case["id"] = "COMPREHENSION_MISSING"
+        case["comprehension_evidence"] = "ABSENT"
         self.assertEqual(MODULE.evaluate(case), "AUTHORIZATION_INADMISSIBLE")
 
+    def test_missing_signature_cannot_be_allowed(self):
+        case = self.base_case()
+        case["signature"] = False
+        self.assertEqual(MODULE.evaluate(case), "SIGNATURE_NOT_ESTABLISHED")
+
     def test_disclosure_drift_requires_review(self):
-        case = {
-            "id": "DISCLOSURE_DRIFT",
-            "presented_hash": "doc:v1",
-            "authorized_hash": "doc:v2",
-            "comprehension_evidence": "DISTINGUISHABLE",
-            "authorization_valid_at_commit": True,
-            "evidence_integrity": True,
-            "temporal_order_valid": True,
-            "minimum_information_satisfied": True,
-            "reconstructable": True,
-        }
+        case = self.base_case()
+        case["id"] = "DISCLOSURE_DRIFT"
+        case["authorized_hash"] = "doc:v2"
         self.assertEqual(MODULE.evaluate(case), "REVIEW")
 
     def test_integrity_failure_fails_closed(self):
-        case = {
-            "id": "EVIDENCE_TAMPER",
-            "presented_hash": "doc:v1",
-            "authorized_hash": "doc:v1",
-            "comprehension_evidence": "DISTINGUISHABLE",
-            "authorization_valid_at_commit": True,
-            "evidence_integrity": False,
-            "temporal_order_valid": True,
-            "minimum_information_satisfied": True,
-            "reconstructable": False,
-        }
+        case = self.base_case()
+        case["id"] = "EVIDENCE_TAMPER"
+        case["evidence_integrity"] = False
+        case["reconstructable"] = False
         self.assertEqual(MODULE.evaluate(case), "FAIL_CLOSED")
 
     def test_lapsed_authorization_denies_requested_effect(self):
-        case = {
-            "id": "AUTHORIZATION_LAPSED",
-            "presented_hash": "doc:v1",
-            "authorized_hash": "doc:v1",
-            "comprehension_evidence": "DISTINGUISHABLE",
-            "authorization_valid_at_commit": False,
-            "evidence_integrity": True,
-            "temporal_order_valid": True,
-            "minimum_information_satisfied": True,
-            "reconstructable": True,
-        }
+        case = self.base_case()
+        case["id"] = "AUTHORIZATION_LAPSED"
+        case["authorization_valid_at_commit"] = False
         self.assertEqual(MODULE.evaluate(case), "DENY")
 
     def test_over_collection_is_not_silently_allowed(self):
-        case = {
-            "id": "OVER_COLLECTION",
-            "presented_hash": "doc:v1",
-            "authorized_hash": "doc:v1",
-            "comprehension_evidence": "DISTINGUISHABLE",
-            "authorization_valid_at_commit": True,
-            "evidence_integrity": True,
-            "temporal_order_valid": True,
-            "minimum_information_satisfied": False,
-            "reconstructable": True,
-        }
+        case = self.base_case()
+        case["id"] = "OVER_COLLECTION"
+        case["minimum_information_satisfied"] = False
         self.assertEqual(MODULE.evaluate(case), "REVIEW_MINIMIZATION")
 
     def test_human_machine_lane_is_comparison_only(self):
